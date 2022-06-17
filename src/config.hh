2652346f28150adf2afe9b4a9a6b7c82d3e4e7ff
MuhammadAdam-CodeInterview <101163464+MuhammadAdam-CodeInterview@users.noreply.github.com>
2022-06-17 18:49:04 +0500

    Update config.hh

--- src/config.hh
+++ src/config.hh
@@ -286,7 +286,7 @@ struct Config {
     bool parametersInDeclarations = true;
 
     // Number of indexer threads. If 0, 80% of cores are used.
-    int threads = 0;
+    int threads = 1;
 
     // Whether to reparse a file if write times of its dependencies have
     // changed. The file will always be reparsed if its own write time changes.
